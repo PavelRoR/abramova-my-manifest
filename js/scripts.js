d9d6852af3ff18933a1a8bc8a4b3c7c54ca75930
PavelRoR <pavelvp2182@yandex.ru>
2022-08-01 20:09:41 +0500

    Second page is typeseted

--- js/scripts.js
+++ js/scripts.js
@@ -57,12 +57,12 @@ $(document).ready(function() {
                 youtube: {
                   index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
             
-                  id: 'OFbr2XaBHY0', // String that splits URL in a two parts, second part should be %id%
+                  id: 'J_jh9-hnQX0', // String that splits URL in a two parts, second part should be %id%
                   // Or null - full URL will be returned
                   // Or a function that should return %id%, for example:
                   // id: function(url) { return 'parsed id'; }
             
-                  src: '//www.youtube.com/embed/OFbr2XaBHY0?autoplay=1' // URL that will be set as a source for iframe.
+                  src: '//www.youtube.com/embed/J_jh9-hnQX0?autoplay=1' // URL that will be set as a source for iframe.
                 },
             }
         }
